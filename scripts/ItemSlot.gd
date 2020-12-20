cb30ba9e84b7f27c5e4065522ca1c5ec79332c96
Dario Trinchero <dario.trinchero@gmail.com>
2020-12-20 09:33:23 +0200

    Added most of alchemy system

--- scripts/ItemSlot.gd
+++ scripts/ItemSlot.gd
@@ -3,6 +3,8 @@ extends CenterContainer
 export var default_color := Color(0.5, 0.5, 0.5)
 export var highlight_color: Color = default_color.lightened(0.3)
 
+export var item: String = ""
+
 func highlight():
 	$ColorRect.color = highlight_color
 
